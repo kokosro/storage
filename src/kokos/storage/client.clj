c21073cd4cee10347d1663174c792806bbd7a882
Robert Doroftei <robert@kokos.ro>
2016-02-24 18:44:54 +0200

    storage server address changed to atom. now db must be instanciated with (set-server! [...])

--- src/kokos/storage/client.clj
+++ src/kokos/storage/client.clj
@@ -5,16 +5,19 @@
   (:gen-class))
 
 
-(def ^:dynamic *server-address* ["" nil])
+(def *server-address* (atom ["" nil]))
+
+(defn set-server! [server-address]
+  (reset! *server-address* server-address))
 
 (defmacro with-server [server-address & body]
   `(binding [*server-address* ~server-address]
      ~@body))
 
 (defn create [type-path data]
-  (let [response (http/post (str (first *server-address*) "/" type-path "/0")
+  (let [response (http/post (str (first @*server-address*) "/" type-path "/0")
                             {:form-params data
-                             :basic-auth (second *server-address*)})
+                             :basic-auth (second @*server-address*)})
         response-body (try (apply str (map char (.bytes (:body @response))))
                        (catch Exception e nil))]
     (if (nil? response-body)
@@ -22,9 +25,9 @@
       (edn/read-string response-body))))
 
 (defn update [type-path update-info id]
-  (let [response (http/post (str (first *server-address*) "/" type-path "/" id)
+  (let [response (http/post (str (first @*server-address*) "/" type-path "/" id)
                             {:form-params update-info
-                             :basic-auth (second *server-address*)})
+                             :basic-auth (second @*server-address*)})
         response-body (try (apply str (map char (.bytes (:body @response))))
                        (catch Exception e nil))]
     (if (nil? response-body)
@@ -34,10 +37,10 @@
 (defn search [type-path  & {:keys [id query] :or {id nil
                                                   query "*:*"}}]
   (let [response (if-not (nil? id) 
-                   (http/get (str (first *server-address*) "/" type-path "/" id)
-                             {:basic-auth (second *server-address*)})
-                   (http/get (str (first *server-address*) "/" type-path "/_search")
+                   (http/get (str (first @*server-address*) "/" type-path "/" id)
+                             {:basic-auth (second @*server-address*)})
+                   (http/get (str (first @*server-address*) "/" type-path "/_search")
                              {:query-params {:q query}
-                              :basic-auth (second *server-address*)}))]
+                              :basic-auth (second @*server-address*)}))]
     (edn/read-string (try (apply str (map char (.bytes (:body @response))))
                        (catch Exception e nil)))))
